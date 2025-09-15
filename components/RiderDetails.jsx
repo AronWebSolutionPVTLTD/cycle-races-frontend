db38b4af5bf10c6e87006a057b3cc2b34c067f96
manisharon1998 <manish.aron1998@gmail.com>
2025-09-15 10:17:06 +0530

    pull

--- components/RiderDetails.jsx
+++ components/RiderDetails.jsx
@@ -1,26 +1,25 @@
 const RiderDetails = ({ rider }) => (
-    <div className="col-lg-12">
-      <div className="top-wraper">
-        <ul className="breadcrumb">
-          <li><a href="/">home</a></li>
-          <li><a href="/riders">riders</a></li>
-          <li>{rider.name}</li>
-        </ul>
-        <div className="wraper">
-          <img src={rider.image} alt={rider.name} />
-          <h1>{rider.name}</h1>
-        </div>
-        <ul className="plyr-dtls">
-          <li>
-            <img src="/images/flag1.svg" alt="Country" />
-            <span>{rider.country}</span>
-          </li>
-          <li>{rider.dob} ({rider.age})</li>
-          <li>{rider.city}</li>
-        </ul>
+  <div className="col-lg-12">
+    <div className="top-wraper">
+      <ul className="breadcrumb">
+        <li><a href="/">home</a></li>
+        <li><a href="/riders">riders</a></li>
+        <li>{rider.name}</li>
+      </ul>
+      <div className="wraper">
+        <img src={rider.image} alt={rider.name} />
+        <h1>{rider.name}</h1>
       </div>
+      <ul className="plyr-dtls">
+        <li>
+          <img src="/images/flag1.svg" alt="Country" />
+          <span>{rider.country}</span>
+        </li>
+        <li>{rider.dob} ({rider.age})</li>
+        <li>{rider.city}</li>
+      </ul>
     </div>
-  );
-  
-  export default RiderDetails;
-  
+  </div>
+);
+
+export default RiderDetails;
